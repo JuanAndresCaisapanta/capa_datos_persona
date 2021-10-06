6ccabe260e60412c11f1448bc39b64bf4855170f
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2021-10-06 02:50:44 -0500

    Clase persona dao con crud implementado

--- persona.py
+++ persona.py
@@ -30,7 +30,7 @@ class Persona:
 
     @property
     def apellido(self):
-        return self._nombre
+        return self._apellido
     @apellido.setter
     def apellido(self, apellido):
         self._apellido=apellido
